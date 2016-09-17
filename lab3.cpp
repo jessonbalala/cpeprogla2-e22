476c20305fca3d4f2b743e589bbad2fed8715857
jessonbalala <jessonbalala@yahoo.com>
2016-09-17 10:10:26 +0800

    lab 3 update

--- lab3.cpp
+++ lab3.cpp
@@ -1,36 +1,201 @@
-#include<iostream>
 #include<cstring>
+
+
 using namespace std;
 
-int myStrCmp(char* str1,char* str2){
-  //return strlen(str);
- switch(strcmp(str1,str2)){
- 	
+char* myStrCmp( char* str1, char* str2){
+	
+switch (strcmp (str1, str2)){
 	case -1:
-		cout<<"negative";
+	cout << "Negative";
+	break;
 	case 0:
-		cout<<"equal";
+	cout << "Equal";
+	break;
 	case 1:
-		cout<<"positive";
-		break;
- }
+	cout << "Positive";
+	break;
+	}
 }
 
-void myStrCpy(char* str1,char* str2){
-	strcpy(str1,str2);
+//Problem #01
+int problem1(){
+	
+	cout << "******************" << endl;
+	cout << "STRING COMPARE" << endl;
+	cout << "******************" << endl;
+	
+	char str1[30], str2[30];
+	cout << "Enter a first word(str1): ";
+	cin >> str1;
+	
+	cout << "Enter a second word(str2): ";
+	cin >> str2;
+	
+	cout<<endl;
+	
+	myStrCmp(str1, str2);
+
 }
-void myStrCat(char* str1,char* str2){
-	strcpy(str1,str2);
+
+//Problem #02
+int problem2(){
+	
+	cout << endl << "******************" << endl;
+	cout << "STRING COPY" << endl;
+	cout << "******************" << endl;
+	
+	char str1[30], str2[30];
+	cout << "Enter a first word(str1): ";
+	cin.ignore();
+	cin.getline(str1,30);
+	cout << "Enter a second word(str2): ";
+	cin.getline(str2,30);
+	
+	strcpy (str1, str2);
+	cout << "New string value for str1: " << str1;
+	
 }
-int main(){
-  char str1[100]="hello";
-  char str2[100]="Hello";
-  
-  
-  myStrCmp(str1,str2);
-  myStrCpy(str1,str2);
-  myStrCat(str1,str2);
-  cout << str1;
-  
-  
+
+
+//Problem #03
+int problem3(){
+
+	cout << endl << "******************" << endl;
+	cout << "STRING CONCATINATION" << endl;
+	cout << "******************" << endl;
+	
+	char str1[30], str2[30];
+	
+	cout << "Enter a first word(str1): ";
+	cin.getline(str1,100);
+	
+	cout << "Enter a second word(str2): ";
+	cin.getline(str2,30);
+	
+	strcat(str1,str2);
+	cout<<str1;
+}
+
+
+
+//Problem #04
+
+int problem4(){
+
+	cout << "******************" << endl;
+	cout << "PALINDROME" << endl;
+	cout << "******************" << endl;
+	
+	char str[30];
+	int i, length;
+	int palindrome = 0;
+	
+	
+	cout << "Enter a word: ";
+	cin.getline(str, 30);
+	
+	length = strlen(str);
+	
+	for (i=0; i<length; i++){
+	if(str[i] != str[length-i-1]){
+	palindrome = 1;
+	}
+	}
+	
+	if (palindrome ){
+	cout << str << " is not a palindrome" << endl;
+	}
+	else {
+	cout << str << " is a palindrome" << endl;
+	}
+}
+
+//Problem #05
+
+
+int problem5(){
+	
+
+char str[30];
+	int i;
+	
+	cout<<"Enter some string: ";
+	cin.getline(str, 30);
+	cout<<endl;
+	
+	
+	str[0]= toupper(str[0]);
+	
+	for(i=1;i<strlen(str);i++){
+	
+	
+	if(isspace(str[i])){
+	
+	i++;	
+	str[i]=toupper(str[i]);	
+	
+	}	
+	
+	else	
+	
+	str[i]=tolower(str[i]);	
+	
+	}
+	
+	cout<<str[0];
+	
+	for(i=1;i<strlen(str);i++){
+	
+	cout<<str[i];
+	
+	}
+	
+}
+
+
+//Problem #06
+
+int problem6(){
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+main(){	
+
+
+	problem1();
+	cout<<endl;
+	
+	problem2();
+	cout<<endl;
+	
+	problem3();
+	cout<<endl;
+	
+	problem4();
+	cout<<endl;
+	
+	problem5();
+	cout<<endl;
+	
+	
 }
