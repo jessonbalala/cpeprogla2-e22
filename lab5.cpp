add5b9002c19b25ede3745e0cbcc1ef211998113
jessonbalala <jessonbalala@yahoo.com>
2016-09-17 12:19:59 +0800

    Add files via upload

--- lab5.cpp
+++ lab5.cpp
@@ -1,43 +1,53 @@
 //Balala
+//problem 1
 #include <iostream>
 using namespace std;
 
 struct Name{
-       char lastName[30];
-       char firstName[30];
+       char fullName[30];
 };
 
 struct Student{
        int id;
        Name name;
-       double grade[3];       
+       double quiz[3];       
 };
 
 void newLine();
 
 int main(){
     Student stud;
+    double sum;
     cout << "Enter student records: \n";
     cout << "ID: ";
     cin >> stud.id;
     newLine();
-    cout << "First Name: ";
-    cin.getline(stud.name.firstName, 29);
-    cout << "Last Name: ";
-    cin.getline(stud.name.lastName, 29);
+    cout << "Name: ";
+    cin.getline(stud.name.fullName, 29);
+   
     for(int i=0; i<3; i++){
-            cout << "Grade "<< i << ": ";
-            cin >> stud.grade[i];        
+            cout << "Quiz "<< i+1 << ": ";
+            cin >> stud.quiz[i]; 
+			sum += stud.quiz[i]/3;       
     }
-    
-    cout << "\n\n";
+    if(sum>=75){
+		cout << "\n\n";
+    	cout << "Display student records\n";
+    	cout << "ID: " << stud.id << endl;
+    	cout << "Name: " << stud.name.fullName << endl;
+    	cout << "Grades: "<<sum<<endl;
+    	cout << "Remarks :Passed"<<endl;    
+    }
+	
+	else{
+		 cout << "\n\n";
     cout << "Display student records\n";
     cout << "ID: " << stud.id << endl;
-    cout << "First Name: " << stud.name.firstName << endl;
-    cout << "Last Name: " << stud.name.lastName << endl;
-    for(int i=0; i<3; i++){
-            cout << "Grade "<< i << ": " << stud.grade[i] << endl;        
+    cout << "First Name: " << stud.name.fullName << endl;
+    cout << "Grades: "<<sum<<endl;
+    cout << "Remarks: Failed"<<endl;
     }
+    
     
     system("pause > 0");
     return 0;   
