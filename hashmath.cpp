e13da3b4b730f03e719963ba708fe60ad93a0fde
jessonbalala <jessonbalala@yahoo.com>
2016-09-26 20:23:28 +0800

    Create hashmath.cpp

---
+++ hashmath.cpp
@@ -0,0 +1,19 @@
+//Balala,Jesson
+
+#include<iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+int main(){
+	int a;
+	int b;
+	int c;
+	cout<<"Hashmat Allies:";
+	cin>>a;
+	cout<<"Hashmat Enemy:";
+	cin>>b;
+	
+	c = abs(a-b);
+	cout<<c;
+}
