ed69b1fae03b3ad9ac0df3e7ccd4e4eae4f7b4dc
jessonbalala <jessonbalala@yahoo.com>
2016-09-26 20:25:11 +0800

    Create fizzbuzz.cpp

---
+++ fizzbuzz.cpp
@@ -0,0 +1,25 @@
+//Balala, Jesson U.
+
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main (){	
+
+	for( int a = 1; a <= 100; a++ )
+
+	if (a % 3==0 && a % 5==0){
+		cout<<"FizzBuzz!"<<endl;
+	}
+	else if (a % 3==0){
+		cout<<"Fizz!"<<endl;
+	}
+	else if (a % 5==0){
+		cout<<"Buzz!"<<endl;
+	}
+	else {
+		cout<<a<<endl;
+	}
+		
+}
